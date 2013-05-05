02e7eb36b586a3c7cdbd0ed3892e2445e17ae0fd
Ivan Zotov <ivanzotov@gmail.com>
2013-05-05 23:49:17 +0400

    Update dragonfly

--- pages/config/initializers/dragonfly.rb
+++ pages/config/initializers/dragonfly.rb
@@ -1,6 +1,9 @@
 require 'dragonfly/rails/images'
 
 app = Dragonfly[:images]
+app_images.configure_with(:imagemagick)
+app.configure_with(:rails)
+
 app.configure do |c|
   c.url_format = '/images/:job/:basename.:format'
 end
