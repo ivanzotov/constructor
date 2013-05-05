2e32391127ffa737aea17e892463dfa6599a318a
Ivan Zotov <ivanzotov@gmail.com>
2013-05-05 23:51:55 +0400

    Update

--- pages/config/initializers/dragonfly.rb
+++ pages/config/initializers/dragonfly.rb
@@ -1,7 +1,7 @@
 require 'dragonfly/rails/images'
 
 app = Dragonfly[:images]
-app_images.configure_with(:imagemagick)
+app.configure_with(:imagemagick)
 app.configure_with(:rails)
 
 app.configure do |c|
