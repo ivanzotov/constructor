e4894a5f622e059fccc37194da2e3cc261b381cd
Ivan Zotov <ivanzotov@gmail.com>
2013-05-27 19:15:17 +0400

    using font-awesome

--- core/config/environments/development.rb
+++ core/config/environments/development.rb
@@ -1,5 +1,4 @@
 Rails.application.class.configure do
-  config.assets.precompile += ['ckeditor/*']
   config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
   config.assets.precompile += %w( .svg .eot .woff .ttf )
 end
