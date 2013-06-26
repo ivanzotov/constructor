4c835381477e7b8049be07b32c3f94cfec3cbc6d
Ivan Zotov <ivanzotov@gmail.com>
2013-06-26 22:14:19 +0400

    Delete remove_list from urlify

--- core/app/assets/javascripts/constructor_core/urlify.js
+++ core/app/assets/javascripts/constructor_core/urlify.js
@@ -12,6 +12,7 @@ var RUSSIAN_MAP = {
 }
 
 var ALL_DOWNCODE_MAPS=new Array()
+
 ALL_DOWNCODE_MAPS[0]=RUSSIAN_MAP
 
 var Downcoder = new Object();
@@ -61,19 +62,11 @@ downcode= function( slug )
     return downcoded;
 }
 
-
 function URLify(s, num_chars) {
     // changes, e.g., "Petty theft" to "petty_theft"
     // remove all these words from the string before urlifying
     s = downcode(s);
-    removelist = ["a", "an", "as", "at", "before", "but", "by", "for", "from",
-                  "is", "in", "into", "like", "of", "off", "on", "onto", "per",
-                  "since", "than", "the", "this", "that", "to", "up", "via",
-                  "with"];
-    r = new RegExp('\\b(' + removelist.join('|') + ')\\b', 'gi');
-    s = s.replace(r, '');
-    // if downcode doesn't hit, the char will be stripped here
-    s = s.replace(/[^-\w\s]/g, '');  // remove unneeded chars
+    s = s.replace(/[^-\w\s]/g, '-');  // remove unneeded chars
     s = s.replace(/^\s+|\s+$/g, ''); // trim leading/trailing spaces
     s = s.replace(/[-\s]+/g, '-');   // convert spaces to hyphens
     s = s.toLowerCase();             // convert to lowercase
