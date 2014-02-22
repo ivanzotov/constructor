34a21615c1e0c263f2ed864b17244a6ff71953bf
Ivan Zotov <ivanzotov@gmail.com>
2014-02-22 13:33:51 +0400

    Fix trim trailing hyphens

--- pages/app/assets/javascripts/constructor_pages/urlify.js
+++ pages/app/assets/javascripts/constructor_pages/urlify.js
@@ -69,6 +69,7 @@ function URLify(s, num_chars) {
     s = s.replace(/[^-\w\s]/g, '-');  // remove unneeded chars
     s = s.replace(/^\s+|\s+$/g, ''); // trim leading/trailing spaces
     s = s.replace(/[-\s]+/g, '-');   // convert spaces to hyphens
+    s = s.replace(/^-+|-+$/g, '');   // trim leading/trailing hyphens
     s = s.toLowerCase();             // convert to lowercase
     return s.substring(0, num_chars);// trim to first num_chars chars
 }
